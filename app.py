787784262c71a8670648230c021e23efb1454f0c
Shiva Baral <shivabaral500@gmail.com>
2022-11-20 19:58:50 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def helo_world():
-  return "Hellow World"
+  return render_template('home.html')
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0', debug=True)
